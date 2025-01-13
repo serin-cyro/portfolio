99bf85b341433625a2b743dd92895715bfa97a62
Serin Tony <serintony@gmail.com>
2025-01-13 21:05:44 +0530

    a under maintainace

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ProjectsComponent } from "../projects/projects.component"; 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatTooltipModule, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
